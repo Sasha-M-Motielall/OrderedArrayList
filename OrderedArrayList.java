2453e0761cb1c0957800c1b4ff3ada55de846f23
Sasha-M-Motielall <smotielall20@stuy.edu>
2020-12-13 22:01:14 -0500

    add() and findPlace() methods constructed

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,4 +8,27 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
   }
+
+  public boolean add(T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("You cannot add null values");
+    }
+    if (size() > 0 && element.compareTo(super.get(size() - 1)) >= 0) {
+      super.add(element);
+    }
+    else {
+      super.add(findPlace(element), element);
+    }
+    return true;
+  }
+
+  private int findPlace(T element) {
+    int index = 0;
+    for (int i = 0; i < size() - 1; i++) {
+      if (get(i).compareTo(element) <= 0 && get(i + 1).compareTo(element) >= 0) {
+        index = i + 1;
+      }
+    }
+    return index;
+  }
 }
